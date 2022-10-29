c26f77f69ac04401ac9323b01e6a47f5f09047e3
LeeWoojin-99 <lwj1316@naver.com>
2022-10-29 17:16:39 +0900

    1029

--- ezen/Dashboard.js
+++ ezen/Dashboard.js
@@ -1,7 +1,15 @@
 var list = document.querySelectorAll("#navigation li");
 function activeLink(){
-    list.forEach(item =>
-    item.classList.remove("hovered"));
+    list.forEach(i => i.classList.remove("hovered"));
     this.classList.add("hovered");
 }
-list.forEach(item => item.addEventListener("mouseover", activeLink));
+list.forEach(i => i.addEventListener("mouseover", activeLink));
+
+var menu = document.querySelectorAll(".toggle");
+var curve = document.querySelectorAll("#navigation ul li.hovered");
+var main = document.querySelectorAll("#main");
+function toggleMenu(){
+    main.forEach(i => i.classList.toggle("off"));
+    curve.forEach(i => i.classList.toggle("off"));
+}
+menu.forEach(i => i.addEventListener("click", toggleMenu));
